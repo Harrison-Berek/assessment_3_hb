25b6257592f2ab822955e183df9463da27e49057
Harrison-Berek <hcberek@gmail.com>
2021-04-23 12:25:07 -0600

    Complete Step 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Item(models.Model):
+    item = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.item
+    
+    def get_absolute_url(self):
+        return reverse('home')
 
 # Create your models here.
